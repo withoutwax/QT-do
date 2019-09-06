14f57f92c6507f7d8d542db9fe287d3d0e2011d2
Will <rlagmlckd@gmail.com>
2019-09-06 00:33:26 -0500

    Setting the Chart to start on proper day (Jan 01)

--- src/chart/index.js
+++ src/chart/index.js
@@ -11,6 +11,7 @@ const BOX_HEIGHT = 10;
 const CANVAS_WIDTH = 10*70;
 const CANVAS_HEIGHT = 10*10;
 const BOX_COLOR = themes.standard.level0;
+const THEME_01 = themes.standard.level1;
 
 function chart(data) {
     let canvas = document.querySelector('.chart');
@@ -20,9 +21,13 @@ function chart(data) {
         let ctx = canvas.getContext('2d');
 
         let day;
-        let yearStartDay = moment().dayOfYear(1).format('d');
+        let yearStartDay = Number(moment().dayOfYear(1).format('d'));
         let week;
         let canvas_index = 1; // Finding the xth day of the year for canvas
+        let today = moment();
+
+        console.log(typeof yearStartDay);
+        
 
         ctx.canvas.width  = CANVAS_WIDTH;
         ctx.canvas.height = CANVAS_HEIGHT;
@@ -30,31 +35,45 @@ function chart(data) {
         // console.log(data[0].date[14]);
         // console.log(moment(undefined))
         let dateIndex = 1;
-        for (week = 1; week <= 52; week++) {
+
+        for (week = 1; week <= 53; week++) {
             for (day = 1; day <= 7; day++) {
 
-                ctx.fillStyle = BOX_COLOR;
-                ctx.fillRect((week*(BOX_WIDTH + 3)), (day*(BOX_HEIGHT + 3)), BOX_WIDTH, BOX_WIDTH);
+                // Setting the start date (eg - Mon, Tues etc)
+                if (week === 1 && day === 1) {
+                    day += yearStartDay;
+                }
+                // console.log(typeof day);
 
-                if (calculateDays(data[0].date, dateIndex) === canvas_index) {
+                
+                // ctx.fillRect((week*(BOX_WIDTH + 3)), (day*(BOX_HEIGHT + 3)), BOX_WIDTH, BOX_WIDTH);
+
+                if (calculateDays(data[0].date, dateIndex) === canvas_index) { // Did Exercise
                     // console.log("red_index", canvas_index);
-                    ctx.fillStyle = '#ff0000';
+                    ctx.fillStyle = THEME_01;
                     dateIndex += 1;
-                } else if (calculateDays(['09/05/2019'], 0) === canvas_index) {
+                } else if (calculateDays([today], 0) === canvas_index) {
                     // console.log("today!");
                     ctx.fillStyle = '#32a852';
+                } else if (calculateDays([today], 0) < canvas_index) {
+                    ctx.fillStyle = '#fcba03';
                 } else {
                     // console.log("canvas_index", canvas_index);
-                    ctx.fillStyle = '#000';
+                    ctx.fillStyle = BOX_COLOR;
                 }
 
-                ctx.fillText(canvas_index, (week*(BOX_WIDTH + 3)), (day*(BOX_HEIGHT + 3)));
-
+                ctx.fillRect((week*(BOX_WIDTH + 3)), (day*(BOX_HEIGHT + 3)), BOX_WIDTH, BOX_WIDTH);
+                // ctx.fillText(canvas_index, (week*(BOX_WIDTH + 3)), (day*(BOX_HEIGHT + 3)));
+                console.log(canvas_index);
                 canvas_index += 1;
+                if (canvas_index > 365) {
+                    console.log("NEXT YEAR");
+                    break;
+                }
                 
             }
         }
-
+        
     }
 }
 
